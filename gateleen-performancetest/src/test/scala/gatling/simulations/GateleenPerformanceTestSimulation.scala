c6f7f0f0e3cbf88c3d5456ed8491e07afc3cf7b0
Marc-André Weber <maweber84@gmail.com>
2016-06-16 17:22:47 +0200

    reactivated tests scenarios

--- gateleen-performancetest/src/test/scala/gatling/simulations/GateleenPerformanceTestSimulation.scala
+++ gateleen-performancetest/src/test/scala/gatling/simulations/GateleenPerformanceTestSimulation.scala
@@ -17,20 +17,17 @@ class GateleenPerformanceTestSimulation extends Simulation {
   }
 
   setUp(
-    Scenarios.storageOperations.inject(constantUsersPerSec(10) during(5 seconds))
-
-//    Scenarios.storageOperations.inject(constantUsersPerSec(35) during(60 seconds)),
-//    Scenarios.prepareExpandResources.inject(nothingFor(60 seconds), atOnceUsers(1)),
-//    Scenarios.regularExpand.inject(nothingFor(2 minutes), constantUsersPerSec(47) during(15 minutes)),
-//    Scenarios.storageExpand.inject(nothingFor(17 minutes), constantUsersPerSec(35) during(20 minutes)),
-//    Scenarios.enqueueRequests.inject(nothingFor(37 minutes), constantUsersPerSec(10) during(2 minutes)),
-//    Scenarios.checkQueuesEmpty.inject(nothingFor(43 minutes), atOnceUsers(1))
+    Scenarios.storageOperations.inject(constantUsersPerSec(35) during(60 seconds)),
+    Scenarios.prepareExpandResources.inject(nothingFor(60 seconds), atOnceUsers(1)),
+    Scenarios.regularExpand.inject(nothingFor(2 minutes), constantUsersPerSec(47) during(15 minutes)),
+    Scenarios.storageExpand.inject(nothingFor(17 minutes), constantUsersPerSec(35) during(20 minutes)),
+    Scenarios.enqueueRequests.inject(nothingFor(37 minutes), constantUsersPerSec(10) during(2 minutes)),
+    Scenarios.checkQueuesEmpty.inject(nothingFor(43 minutes), atOnceUsers(1))
   )
     .protocols(httpConf)
     .assertions(
       global.successfulRequests.percent.is(100),
-      global.responseTime.max.lessThan(2)
-//      global.responseTime.max.lessThan(10)
+      global.responseTime.max.lessThan(2000)
     )
 
 }
