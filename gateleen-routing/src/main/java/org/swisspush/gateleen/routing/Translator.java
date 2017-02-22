0f0f7b7a9f4a92f80fcebec9f117fb4290de7106
Ljuca Mario <mario.ljuca@gmail.com>
2017-02-22 11:13:07 +0100

    fixed #117 ResourceCopyHandler stuck in queue with x-status-translate-4xx header

--- gateleen-routing/src/main/java/org/swisspush/gateleen/routing/Translator.java
+++ gateleen-routing/src/main/java/org/swisspush/gateleen/routing/Translator.java
@@ -1,50 +1,18 @@
 package org.swisspush.gateleen.routing;
 
-import io.vertx.core.MultiMap;
 import org.slf4j.Logger;
+import org.swisspush.gateleen.core.util.StatusCodeTranslator;
 
 import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
- * Helps to translate status codes.
+ * Enhances the {@link org.swisspush.gateleen.core.util.StatusCodeTranslator } with features
+ * needed explicitly in the Router.
  * 
- * @author https://github.com/ljucam [Mario Ljuca]
+ * @author https://github.com/ljucam [Mario Aerni]
  */
-public class Translator {
-
-    /**
-     * Checks if there exists a handling for the given <code>statusCode</code> in the
-     * headers of the request. The header is checked for a pattern matching <code>x-translate-status-</code>
-     * and the given <code>statusCode</code>.
-     * 
-     * @param statusCode the original status code
-     * @param headers the headers of the request
-     * @return the translated status or if no translation was carried out the original status code.
-     */
-    public static int translateStatusCode(int statusCode, MultiMap headers) {
-        String translatedStatus = null;
-
-        // it dosen't make sense to translate status code 200 ...
-        if (statusCode > 200) {
-            translatedStatus = headers.get("x-translate-status-" + statusCode);
-            if (translatedStatus == null) {
-                translatedStatus = headers.get("x-translate-status-" + (statusCode / 100) + "xx");
-            }
-        }
-
-        if (translatedStatus != null) {
-            try {
-                // translated status code
-                return Integer.parseInt(translatedStatus);
-            } catch (NumberFormatException e) {
-                // ignore
-            }
-        }
-
-        // original status code
-        return statusCode;
-    }
+public class Translator extends StatusCodeTranslator {
 
     /**
      * Checks if there exists a handling for the given <code>statusCode</code> in the
