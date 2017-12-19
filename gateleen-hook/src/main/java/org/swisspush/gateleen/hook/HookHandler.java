feddf78c87332f33e926a1452cfce180673c4c93
Tom Winterhalder <tom.winterhalder@gmail.com>
2017-12-19 18:31:44 +0100

    #186 Enqueue directly to destination queue

--- gateleen-hook/src/main/java/org/swisspush/gateleen/hook/HookHandler.java
+++ gateleen-hook/src/main/java/org/swisspush/gateleen/hook/HookHandler.java
@@ -1234,10 +1234,13 @@ public class HookHandler implements LoggableResource {
         extractAndAddStaticHeadersToHook(jsonHook, hook);
 
         Map<String, String> staticHeaders = hook.getStaticHeaders();
-        String destinationQueue = LISTENER_QUEUE_PREFIX + "-" + listenerId;
+        String destinationQueue = null;
         if (staticHeaders != null) {
             // remove static x-queue header. Otherwise it will be re-queued at the end of the queue.
             destinationQueue = hook.getStaticHeaders().remove(X_QUEUE);
+        }
+        if (destinationQueue == null){
+            destinationQueue = LISTENER_QUEUE_PREFIX + "-" + listenerId;
         }
 
         /*
