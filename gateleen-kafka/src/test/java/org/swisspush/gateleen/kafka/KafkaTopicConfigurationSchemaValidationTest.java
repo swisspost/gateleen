3f13b84f2cb918579fa1a3793d4e4ee8c5d8b5b0
Marc-André Weber <maweber84@gmail.com>
2019-09-12 10:52:55 +0200

    #291 Add json schema for kafka topics configuration

---
+++ gateleen-kafka/src/test/java/org/swisspush/gateleen/kafka/KafkaTopicConfigurationSchemaValidationTest.java
@@ -0,0 +1,66 @@
+package org.swisspush.gateleen.kafka;
+
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.swisspush.gateleen.core.util.ResourcesUtils;
+import org.swisspush.gateleen.core.validation.ValidationResult;
+import org.swisspush.gateleen.core.validation.ValidationStatus;
+import org.swisspush.gateleen.validation.Validator;
+
+/**
+ * Tests for the logging resource schema
+ *
+ * @author https://github.com/mcweba [Marc-Andre Weber]
+ */
+@RunWith(VertxUnitRunner.class)
+public class KafkaTopicConfigurationSchemaValidationTest {
+
+    private final String CONFIG_SCHEMA = ResourcesUtils.loadResource("gateleen_kafka_topic_configuration_schema", true);
+    private final String VALID_CONFIG_RESOURCE = ResourcesUtils.loadResource("testresource_valid_kafka_topic_configuration", true);
+    private final String MISSING_BOOTSTRAP_SERVERS_CONFIG_RESOURCE = ResourcesUtils.loadResource("testresource_kafka_topic_configuration_missing_bootstrap_servers", true);
+    private final String INVALID_TYPES_CONFIG_RESOURCE = ResourcesUtils.loadResource("testresource_kafka_topic_configuration_invalid_types", true);
+
+    private Logger logger = LoggerFactory.getLogger(KafkaTopicConfigurationSchemaValidationTest.class);
+
+    @Test
+    public void testValidConfig(TestContext context){
+        ValidationResult validationResult = validate(VALID_CONFIG_RESOURCE);
+        context.assertNotNull(validationResult);
+        context.assertEquals(ValidationStatus.VALIDATED_POSITIV, validationResult.getValidationStatus());
+    }
+
+    @Test
+    public void testMissingBootstrapServersConfig(TestContext context){
+        ValidationResult validationResult = validate(MISSING_BOOTSTRAP_SERVERS_CONFIG_RESOURCE);
+        context.assertNotNull(validationResult);
+        context.assertEquals(ValidationStatus.VALIDATED_NEGATIV, validationResult.getValidationStatus());
+        context.assertEquals("Validation failed", validationResult.getMessage());
+        context.assertEquals("$.my.topic.*.bootstrap.servers: is missing but it is required", extractErrorMessage(validationResult));
+    }
+
+    @Test
+    public void testInvalidTypesConfig(TestContext context){
+        ValidationResult validationResult = validate(INVALID_TYPES_CONFIG_RESOURCE);
+        context.assertNotNull(validationResult);
+        context.assertEquals(ValidationStatus.VALIDATED_NEGATIV, validationResult.getValidationStatus());
+        context.assertEquals("Validation failed", validationResult.getMessage());
+        context.assertEquals("$.*.someIntegerProperty: integer found, string expected", extractErrorMessage(validationResult));
+    }
+
+    private String extractErrorMessage(ValidationResult validationResult){
+        return validationResult.getValidationDetails().getJsonObject(0).getString("message");
+    }
+
+    private String extractErrorValue(ValidationResult validationResult){
+        return validationResult.getValidationDetails().getJsonObject(0).getString("value");
+    }
+
+    private ValidationResult validate(String loggingResource){
+        return Validator.validateStatic(Buffer.buffer(loggingResource), CONFIG_SCHEMA, logger);
+    }
+}
