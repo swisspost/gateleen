50aa5ef5f09c746207534ba4ba7d26212cc1e147
Edouard Coissy <edouard.coissy@post.ch>
2021-01-08 08:01:18 +0100

    parallel composition

--- gateleen-kafka/src/main/java/org/swisspush/gateleen/kafka/KafkaMessageSender.java
+++ gateleen-kafka/src/main/java/org/swisspush/gateleen/kafka/KafkaMessageSender.java
@@ -1,56 +1,47 @@
 package org.swisspush.gateleen.kafka;
 
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.kafka.client.producer.KafkaProducer;
 import io.vertx.kafka.client.producer.KafkaProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class KafkaMessageSender {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaMessageSender.class);
 
     Future<Void> sendMessages(KafkaProducer<String, String> kafkaProducer,
-                                     List<KafkaProducerRecord<String, String>> messages){
+                              List<KafkaProducerRecord<String, String>> messages) {
         Future<Void> future = Future.future();
-        List<Future<Void>> futureList = new ArrayList<>();
+        log.debug("Start processing {} messages for kafka", messages.size());
 
-        for (KafkaProducerRecord<String, String> message : messages) {
-            log.debug("Start processing {} messages for kafka", messages.size());
-            Future<Void> futureEntry = sendMessage(kafkaProducer, message);
-            futureList.add(futureEntry);
-        }
+        @SuppressWarnings("rawtypes") //https://github.com/eclipse-vertx/vert.x/issues/2627
+        List<Future> futures = messages.stream()
+                .map(message -> KafkaMessageSender.this.sendMessage(kafkaProducer, message))
+                .collect(toList());
 
-        kafkaProducer.flush(event -> {});
-
-        final int[] remaining = {futureList.size()};
-
-        for (Future<Void> voidFuture : futureList) {
-           voidFuture.setHandler(event -> {
-               if (event.succeeded()) {
-                   remaining[0] -= 1;
-
-                   if (remaining[0] == 0) {
-                       future.complete();
-                       log.debug("Batch messages successfully sent to kafka.");
-                   }
-               } else {
-                   future.fail(event.cause());
-               }
-           });
-        }
+        CompositeFuture.all(futures).<Void>mapEmpty().setHandler(result -> {
+            if (result.succeeded()) {
+                future.complete();
+                log.debug("Batch messages successfully sent to Kafka.");
+            } else {
+                future.fail(result.cause());
+            }
+        });
 
         return future;
     }
 
-    private Future<Void> sendMessage(KafkaProducer<String, String> kafkaProducer, KafkaProducerRecord<String, String> message){
+    private Future<Void> sendMessage(KafkaProducer<String, String> kafkaProducer, KafkaProducerRecord<String, String> message) {
         Future<Void> f = Future.future();
         kafkaProducer.write(message, event -> {
-            if(event.succeeded()){
-                if (message.key() != null){
+            if (event.succeeded()) {
+                if (message.key() != null) {
                     log.debug("Message with key '{}' successfully sent to kafka. Result: {}", message.key(), event.result().toJson());
                 } else {
                     log.debug("Message without key successfully sent to kafka. Result: {}", event.result().toJson());
