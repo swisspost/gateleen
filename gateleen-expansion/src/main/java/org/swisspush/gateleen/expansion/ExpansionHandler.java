24ce90693534e0579f720e11a7e7a2663a1fba4d
Marc-André Weber <maweber84@gmail.com>
2024-05-02 11:02:05 +0200

    Handle "Payload Too Large" responses from vertx-rest-storage

--- gateleen-expansion/src/main/java/org/swisspush/gateleen/expansion/ExpansionHandler.java
+++ gateleen-expansion/src/main/java/org/swisspush/gateleen/expansion/ExpansionHandler.java
@@ -34,6 +34,7 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.swisspush.gateleen.core.util.StatusCode.INTERNAL_SERVER_ERROR;
+import static org.swisspush.gateleen.core.util.StatusCode.PAYLOAD_TOO_LARGE;
 import static org.swisspush.gateleen.routing.RuleFeatures.Feature.EXPAND_ON_BACKEND;
 import static org.swisspush.gateleen.routing.RuleFeatures.Feature.STORAGE_EXPAND;
 import static org.swisspush.gateleen.routing.RuleProvider.RuleChangesObserver;
@@ -544,8 +545,11 @@ public class ExpansionHandler implements RuleChangesObserver {
                     return;
                 }
                 cRes.bodyHandler(data -> {
-                    if (StatusCode.INTERNAL_SERVER_ERROR.getStatusCode() == cRes.statusCode()) {
-                        String fullResponseBody = data.toString();
+                    String fullResponseBody = data.toString();
+                    if(StatusCode.PAYLOAD_TOO_LARGE.getStatusCode() == cRes.statusCode()) {
+                        log.info("{}: {}: {}", PAYLOAD_TOO_LARGE, targetUri, fullResponseBody);
+                        handler.handle(new ResourceNode(SERIOUS_EXCEPTION, new ResourceCollectionException(fullResponseBody, PAYLOAD_TOO_LARGE)));
+                    } else if (StatusCode.INTERNAL_SERVER_ERROR.getStatusCode() == cRes.statusCode()) {
                         log.error("{}: {}: {}", INTERNAL_SERVER_ERROR, targetUri, fullResponseBody);
                         handler.handle(new ResourceNode(SERIOUS_EXCEPTION, new ResourceCollectionException(fullResponseBody, StatusCode.INTERNAL_SERVER_ERROR)));
                     } else {
