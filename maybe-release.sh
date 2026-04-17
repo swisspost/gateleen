f5351d7765223ea4a7baad9e019e72a24d910997
Marc-André Weber <maweber84@gmail.com>
2026-04-17 11:54:55 +0200

    Update release script and bump version to 2.1.46-SNAPSHOT

--- maybe-release.sh
+++ maybe-release.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -ev
 
-# Ensure we have the complete history including all branches and tags
-git fetch --all --tags --prune
-
-# Detach HEAD to allow updating branch refs
-git checkout --detach
-
-# Now safely fetch branches
-git fetch origin master:master
-git fetch origin develop:develop
-
-# Reset to a clean state on master
-git checkout master
-git reset --hard origin/master
+git fetch
+git reset --hard
 
 # Run the release
 mvn -B -Prelease -PpublicRepos jgitflow:release-start jgitflow:release-finish -DskipTests=true
