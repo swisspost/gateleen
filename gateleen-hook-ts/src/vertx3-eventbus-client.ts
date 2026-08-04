560aefa154d7c057b89a274d90e44d88047a6218
holtzsi <simon.holtz.1@post.ch>
2026-08-04 17:21:15 +0200

    [SDCISA-20256] feat: typescript version of hooks * remove confusing code

--- gateleen-hook-ts/src/vertx3-eventbus-client.ts
+++
@@ -1,6 +0,0 @@
-import EventBus from 'vertx3-eventbus-client';
-import { HookMessage } from './hook-message.ts';
-
-export type MessageHandler<T> = (error: unknown, message: HookMessage<T>) => void;
-
-export default EventBus;
